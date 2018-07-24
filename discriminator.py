daaac1b5221a4142161522c59a7c414b5ec762bb
minhkv <minhqctb194@gmail.com>
2018-07-24 15:58:33 +0800

    update discriminator

--- discriminator.py
+++ discriminator.py
@@ -3,31 +3,32 @@ import numpy as np
 
 lays = tf.layers
 
+def binary_cross_entropy(x, z):
+    eps = 1e-12
+    return (-(x * tf.log(z + eps) + (1. - x) * tf.log(1. - z + eps)))
+
 class Discriminator:
-    def __init__(self, name="", lr = 0.0001):
+    def __init__(self, name="", endpoints={}, lr = 0.0001):
         self.name = name
         self.lr = lr
+        self.endpoints = endpoints
         self._construct_graph()
         self._construct_loss()
         self._construct_summary()
 
-    def model(self, inputs, labels):
+    def model(self, inputs):
         pass
+    
     def _construct_graph(self):
-        self.inputs = tf.placeholder(tf.float32, (None, 32, 32, 1), name="input_{}".format(self.name))  
-        self.labels = tf.placeholder(tf.float32, (None, 10), name="label_{}".format(self.name))  
-        self.logits = self.model(self.inputs, self.labels)
-
+        pass
+        
     def _construct_loss(self):
-        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.labels))
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
-
-        tf.add_to_collection("optimizer_{}".format(self.name), self.optimizer)
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
-        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-
+        pass
+        
     def _construct_summary(self):
-        tf.summary.scalar("loss_d_{}".format(self.name), self.loss)
+        tf.summary.scalar("type_loss_g_{}".format(self.name), self.loss_g_feature)
+        tf.summary.scalar("type_loss_d_{}".format(self.name), self.loss_d_feature)
+        tf.summary.scalar("class_loss_d_{}".format(self.name), self.class_loss)
 
     def init_variable(self):
         # initialize the network
@@ -37,9 +38,5 @@ class Discriminator:
     def merge_all(self):
         self.merged = tf.summary.merge_all()
         self.writer = tf.summary.FileWriter('/tmp/log', self.sess.graph)
+        
     
-    def fit(self, inputs, labels, step):#, sess):
-        labels = self.sess.run(tf.one_hot(labels, depth=10))
-        summary, loss, _ = self.sess.run([self.merged, self.loss, self.optimizer], feed_dict={self.inputs: inputs, self.labels: labels})
-        print("Iter {}: loss={}".format(step, loss))
-        self.writer.add_summary(summary, step)
